025ca823998db509ecf6697a579cfaf97702c8ea
Kamil Frydlewicz <kamil@frydlewicz.pl>
2014-12-07 15:24:45 +0100

    correct string comparison

--- main.c
+++ main.c
@@ -5,11 +5,14 @@
  *      Author: kamil
  */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "daemon.h"
 #include "get.h"
 #include "update.h"
+#include "init.h"
 
 #define DAEMON	"daemon"
 #define GET 	"get"
@@ -17,23 +20,23 @@
 #define INIT	"init"
 
 int main(int argc, char **argv) {
-	if(argc > 1) {
+	if(argc < 2) {
 		return 1;
 	}
 
-	if(argv[1] == DAEMON) {
+	if(strcmp(argv[1], DAEMON) == 0) {
 		return daemon(argc, argv);
 	}
 
-	else if(argv[1] == GET) {
+	else if(strcmp(argv[1], GET) == 0) {
 		return get(argc, argv);
 	}
 
-	else if(argv[1] == UPDATE) {
+	else if(strcmp(argv[1], UPDATE) == 0) {
 		return update(argc, argv);
 	}
 
-	else if(argv[1] == INIT) {
+	else if(strcmp(argv[1], INIT) == 0) {
 		return init(argc, argv);
 	}
 
